e4e2276a346379e60cec730682bcdc1bc0afacfc
Declan <declanscoulding@gmail.com>
2018-08-17 12:44:44 +0100

    new footer added and updates to style.css

---
+++ footer.php
@@ -0,0 +1,26 @@
+    <footer class="height-custom-1 soft-dark">	
+        <div class="height-custom-1">
+            <p class="relative vertical-align left-1 width-94 roboto font-size-0-8 font-weight-400 letter-spacing-0-0-5 text-uppercase"><?php esc_html_e( '&copy;', 'heythere-lite' )?> <?php echo get_the_date( 'Y' ); ?> <?php bloginfo('name')?></p>
+        </div>
+        <?php
+            wp_nav_menu( array(
+                'theme_location' => 'social-menu',
+                'menu_class' => 'social-menu relative',
+                'container' => false,
+                'link_before' => '<span class="screen-reader-text">', // Hide the social links text
+                'link_after' => '</span>', // Hide the social links text
+            ));
+            ?>
+    </footer>
+
+
+    <div class="load-bar width-100 height-100 fixed top-0">
+        <div class="bar relative vertical-align"></div>
+    </div>
+
+    <?php wp_footer(); ?>
+
+</body>
+
+
+</html>
